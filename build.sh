bc98fd183b83fadd052b9dcd113d4b3099b85952
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2018-07-12 12:07:49 +0200

    jdk.incubator.httpclient no longer required

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ ${JAVA_HOME}/bin/jlink \
   --no-man-pages \
   --strip-native-commands \
   --output antbuild/Cryptomator.app/Contents/PlugIns/Java.runtime/Contents/Home \
-  --add-modules java.base,java.logging,java.xml,java.sql,java.management,java.security.sasl,java.naming,java.datatransfer,java.security.jgss,java.rmi,java.scripting,java.prefs,java.desktop,jdk.incubator.httpclient,javafx.fxml,javafx.controls,jdk.incubator.httpclient \
+  --add-modules java.base,java.logging,java.xml,java.sql,java.management,java.security.sasl,java.naming,java.datatransfer,java.security.jgss,java.rmi,java.scripting,java.prefs,java.desktop,javafx.fxml,javafx.controls \
   --verbose
 
 # adjust .app
