426f27a2db1f1aace1334f1dfa6f37b5d4bcfb65
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2019-03-01 14:45:55 +0100

    adjust version in Info.plist

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ TAG_VERSION=${1:-snapshot}
 if [ -z "${JAVA_HOME}" ]; then echo "JAVA_HOME not set. Run using JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-x.y.z.jdk/Contents/Home/ ./build.sh"; exit 1; fi
 if [ ! -x ./tools/packager/jpackager ]; then echo "../tools/packager/jpackager not executable."; exit 1; fi
 if [ ! -x ./tools/create-dmg/create-dmg.sh ]; then echo "./tools/create-dmg/create-dmg.sh not executable."; exit 1; fi
+command -v jq >/dev/null 2>&1 || { echo >&2 "jq not found."; exit 1; }
 command -v curl >/dev/null 2>&1 || { echo >&2 "curl not found."; exit 1; }
 command -v unzip >/dev/null 2>&1 || { echo >&2 "unzip not found."; exit 1; }
 command -v codesign >/dev/null 2>&1 || { echo >&2 "codesign not found. Fix by 'xcode-select --install'."; exit 1; }
@@ -19,8 +20,13 @@ if [ $? -ne 0 ]; then
   echo >&2 "unzipping buildkit.zip failed.";
   exit 1;
 fi
+
+# setting variables
+FIRST_COMMIT='b78ee8295df7f66055b9aaa504c0008aa51ee1d4'
+LATEST_COMMIT=`curl -s "https://api.github.com/repos/cryptomator/cryptomator/git/refs/heads/master" | jq -r '.object.sha'`
+COMMIT_COUNT=`curl -s "https://api.github.com/repos/cryptomator/cryptomator/compare/${FIRST_COMMIT}...${LATEST_COMMIT}" | jq -r '.total_commits'`
 BUILD_VERSION=`cat libs/version.txt`
-echo "Building Cryptomator ${BUILD_VERSION}..."
+echo "Building Cryptomator ${BUILD_VERSION} (${COMMIT_COUNT})..."
 
 # create .app
 ./tools/packager/jpackager create-image \
@@ -49,7 +55,8 @@ cp resources/app/Info.plist app/Cryptomator.app/Contents/
 cp resources/app/Cryptomator.icns app/Cryptomator.app/Contents/Resources/
 cp resources/app/Cryptomator-Vault.icns app/Cryptomator.app/Contents/Resources/
 cp resources/app/libMacFunctions.dylib app/Cryptomator.app/Contents/Java/
-# TODO adjust info.plist contents (version, ...)
+sed -i '' "s|###BUILD_VERSION###|${BUILD_VERSION}|g" app/Cryptomator.app/Contents/Info.plist
+sed -i '' "s|###COMMIT_COUNT###|${COMMIT_COUNT}|g" app/Cryptomator.app/Contents/Info.plist
 
 # prepare .dmg
 cp resources/dmg/FUSE\ for\ macOS.webloc app/
