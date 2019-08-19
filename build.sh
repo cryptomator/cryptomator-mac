ada8d0178a3e42ee45574594a73ebb77e138d6a4
Tobias Hagemann <tobias@hagemann.org>
2019-08-19 11:12:53 +0200

    extracting buildkit into its own directory for easier cleanup

--- build.sh
+++ build.sh
@@ -12,11 +12,11 @@ command -v unzip >/dev/null 2>&1 || { echo >&2 "unzip not found."; exit 1; }
 command -v codesign >/dev/null 2>&1 || { echo >&2 "codesign not found. Fix by 'xcode-select --install'."; exit 1; }
 
 # cleanup
-rm -rf buildkit.zip libs app *.dmg
+rm -rf buildkit.zip buildkit *.dmg
 
 # download buildkit
 curl -o buildkit.zip -L https://dl.bintray.com/cryptomator/cryptomator/${TAG_VERSION}/buildkit-mac.zip
-unzip buildkit.zip
+unzip buildkit.zip -d buildkit
 if [ $? -ne 0 ]; then
   echo >&2 "unzipping buildkit.zip failed.";
   exit 1;
@@ -25,14 +25,14 @@ fi
 # setting variables
 FIRST_COMMIT='b78ee8295df7f66055b9aaa504c0008aa51ee1d4'
 COMMIT_COUNT=$((`curl -s "https://api.github.com/repos/cryptomator/cryptomator/compare/${FIRST_COMMIT}...${GIT_BRANCH}" | jq -r '.total_commits'` + 1))
-BUILD_VERSION=`cat libs/version.txt`
+BUILD_VERSION=`cat buildkit/libs/version.txt`
 echo "Building Cryptomator ${BUILD_VERSION} (${COMMIT_COUNT})..."
 
 # create .app
 ./tools/packager/jpackager create-image \
     --verbose \
     --echo-mode \
-    --input libs \
+    --input buildkit/libs \
     --main-jar launcher-${BUILD_VERSION}.jar  \
     --class org.cryptomator.launcher.Cryptomator \
     --jvm-args "-Dcryptomator.logDir=\"~/Library/Logs/Cryptomator\"" \
@@ -41,7 +41,7 @@ echo "Building Cryptomator ${BUILD_VERSION} (${COMMIT_COUNT})..."
     --jvm-args "-Dcryptomator.mountPointsDir=\"/Volumes\"" \
     --jvm-args "-Xss2m" \
     --jvm-args "-Xmx512m" \
-    --output app \
+    --output buildkit/app \
     --force \
     --identifier org.cryptomator \
     --name Cryptomator \
@@ -51,18 +51,18 @@ echo "Building Cryptomator ${BUILD_VERSION} (${COMMIT_COUNT})..."
     --strip-native-commands
 
 # adjust .app
-cp resources/app/Info.plist app/Cryptomator.app/Contents/
-cp resources/app/Cryptomator.icns app/Cryptomator.app/Contents/Resources/
-cp resources/app/Cryptomator-Vault.icns app/Cryptomator.app/Contents/Resources/
-cp resources/app/libMacFunctions.dylib app/Cryptomator.app/Contents/Java/
-sed -i '' "s|###BUILD_VERSION###|${BUILD_VERSION}|g" app/Cryptomator.app/Contents/Info.plist
-sed -i '' "s|###COMMIT_COUNT###|${COMMIT_COUNT}|g" app/Cryptomator.app/Contents/Info.plist
+cp resources/app/Info.plist buildkit/app/Cryptomator.app/Contents/
+cp resources/app/Cryptomator.icns buildkit/app/Cryptomator.app/Contents/Resources/
+cp resources/app/Cryptomator-Vault.icns buildkit/app/Cryptomator.app/Contents/Resources/
+cp resources/app/libMacFunctions.dylib buildkit/app/Cryptomator.app/Contents/Java/
+sed -i '' "s|###BUILD_VERSION###|${BUILD_VERSION}|g" buildkit/app/Cryptomator.app/Contents/Info.plist
+sed -i '' "s|###COMMIT_COUNT###|${COMMIT_COUNT}|g" buildkit/app/Cryptomator.app/Contents/Info.plist
 
 # prepare .dmg
-cp resources/dmg/FUSE\ for\ macOS.webloc app/
+cp resources/dmg/FUSE\ for\ macOS.webloc buildkit/app/
 
 # codesign
-codesign --force --deep -s 307EF36B2A2EF98EB0AC0D24603A201BBDD4798B app/Cryptomator.app
+codesign --force --deep -s 307EF36B2A2EF98EB0AC0D24603A201BBDD4798B buildkit/app/Cryptomator.app
 if [ $? -ne 0 ]; then
   echo >&2 "codesigning .app failed.";
   exit 1;
@@ -84,4 +84,4 @@ fi
   --eula "resources/dmg/license.rtf" \
   --icon ".background" 128 758 \
   --icon ".fseventsd" 320 758 \
-  Cryptomator-${BUILD_VERSION}.dmg app
+  Cryptomator-${BUILD_VERSION}.dmg buildkit/app
